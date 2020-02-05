01828cafd481f1984b35fa63ae7477a2f377ccd5
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2020-02-05 13:37:37 -0600

    ready for AJAX

--- server/server.js
+++ server/server.js
@@ -5,6 +5,12 @@ const bodyParser = require( 'body-parser' );
 // globals
 const app = express();
 const port = 5000;
+let quotes = [
+    { who: "Denzel", quote: "ease is a larger barrier to progress than hardship" },
+    { who: "Michael Scott", quote: "You miss 100% of the shots you don't take" },
+    { who: "Corey", quote: "woof woof" },
+    { who: "Dev", quote: "fortune cookie BS" }
+];
 
 // uses
 app.use( express.static( 'server/public' ) );
@@ -13,3 +19,8 @@ app.use( express.static( 'server/public' ) );
 app.listen( port, ( req, res )=>{
     console.log( 'server up on:', port );
 }) // end spin up server
+
+app.get( '/quotes', ( req, res )=>{
+    console.log( '/quotes GET hit' );
+    res.send( quotes );
+}) // end /quotes GET
